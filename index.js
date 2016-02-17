ef1b86860f689a3d71ece52ea16ea34c09f7c1f2
moser2010 <cmoser351@gmail.co>
2016-02-17 02:45:22 +0000

    video isues

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("li a").click(function(){
+        $("#About").toggle(function(){
+         $("About").css("display","block");   
+        });
+    });
+});
+$(document).ready(function(){
+    $("#bgvid")[0].autoplay = true;
+})
+
+    
+ 
+         
