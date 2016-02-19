d8956a581d390fbf1ce6dcb477355b6c30dd1f96
moser2010 <cmoser351@gmail.co>
2016-02-19 20:14:18 +0000

    more responsive fixes

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 $(document).ready(function() {
     $(".About").click(function() {
-        $("#bgvid, .position").css('display','none');
-        $("#About").css('display','block');
-        
-        
+        $("#bgvid, .position").css('display', 'none');
+        $("#About").css('display', 'block');
+
+
     });
-    $('.Home').click(function(){
-        $("#bgvid, .position").css('display','block');
-        $('#About').css('display','none')
+    $('.Home').click(function() {
+        $("#bgvid, .position").css('display', 'block');
+        $('#About').css('display', 'none')
         $("#bgvid")[0].autoplay = true;
     });
-    $(".eduButton").click(function(){
-        $('.me').css('display','none')
-        $('.edu').css('display','block')
-        $('.exp').css('display','none')
+    $(".eduButton").click(function() {
+        $('.me').css('display', 'none')
+        $('.edu').css('display', 'block')
+        $('.exp').css('display', 'none')
     });
-    $(".meButton").click(function(){
-        $('.me').css('display','block')
-        $('.edu').css('display','none')
-        $('.exp').css('display','none')
+    $(".meButton").click(function() {
+        $('.me').css('display', 'block')
+        $('.edu').css('display', 'none')
+        $('.exp').css('display', 'none')
     });
-    $(".expButton").click(function(){
-        $('.me').css('display','none')
-        $('.edu').css('display','none')
-        $('.exp').css('display','block')
+    $(".expButton").click(function() {
+        $('.me').css('display', 'none')
+        $('.edu').css('display', 'none')
+        $('.exp').css('display', 'block')
     });
 });
 $(document).ready(function() {
-    $("#bgvid")[0].autoplay = true;
-})
+    $(window).resize(function() {
 
+        if ($(window).width() >= 501) {
+            
+            // is mobile device
 
+        }
+
+    });
+});;                
