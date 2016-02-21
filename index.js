30b6d5d10fbd2d655ab888a6b2bf4182eaefb5c5
moser2010 <cmoser351@gmail.co>
2016-02-21 19:54:00 +0000

    all done

--- index.js
+++ index.js
@@ -3,39 +3,48 @@ $(document).ready(function() {
         $("#bgvid, .position").css('display', 'none');
         $("#About").css('display', 'block');
         $('.videoBG').css('display', 'none')
-
-
+        $('.contactPage').css('display', 'none')
     });
     $('.Home').click(function() {
         $("#bgvid, .position").css('display', 'block');
         $('#About').css('display', 'none');
         $('.videoBG').css('display', 'block')
+        $('.contactPage').css('display', 'none')
         $("#bgvid")[0].autoplay = true;
     });
     $(".eduButton").click(function() {
         $('.me').css('display', 'none')
         $('.edu').css('display', 'block')
         $('.exp').css('display', 'none')
+        $('.contactPage').css('display', 'none')
     });
     $(".meButton").click(function() {
         $('.me').css('display', 'block')
         $('.edu').css('display', 'none')
         $('.exp').css('display', 'none')
+        $('.contactPage').css('display', 'none')
     });
     $(".expButton").click(function() {
         $('.me').css('display', 'none')
         $('.edu').css('display', 'none')
+        $('.contactPage').css('display', 'none')
         $('.exp').css('display', 'block')
     });
+    $('.contactButton').click(function() {
+       $("#bgvid, .position").css('display', 'none');
+        $("#About").css('display', 'none');
+        $('.videoBG').css('display', 'none')
+        $('.contactPage').css('display', 'none')
+        $('.contactPage').css('display', 'block')
+    })
 });
-$(document).ready(function() {
-    $(window).resize(function() {
 
-        if ($(window).width() >= 501) {
-            
-            // is mobile device
-
-        }
-
-    });
-});;                
+$(window).resize(function() {
+    var width = $(window).width();
+    if (width < 960) {
+        //Do Something
+    }
+    else {
+        //Do Something Else
+    }
+});
